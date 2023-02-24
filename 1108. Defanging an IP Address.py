bb4720256dfdbdd705880f16ec9b24cee6c4e20f
Srinivas Aravind <125225286+itzAravind@users.noreply.github.com>
2023-02-24 11:57:43 +0000

    Delete 1108. Defanging an IP Address.py

--- 1108. Defanging an IP Address.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def defangIPaddr(self, address: str) -> str:
-        return address.replace(".","[.]")
-# BruteForce
-#        id_list = list(address)
-#        for i in range(len(id_list)):
-#            if id_list[i] == ".":
-#                id_list[i] = "[.]"
-#        return ("".join(id_list))
