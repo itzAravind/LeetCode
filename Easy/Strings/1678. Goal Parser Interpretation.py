b6cc1c058854c33cafa62584202a8c14cc382748
Srinivas Aravind <125225286+itzAravind@users.noreply.github.com>
2023-02-24 12:27:01 +0000

    Update 1678. Goal Parser Interpretation.py

--- Easy/Strings/1678. Goal Parser Interpretation.py
+++ Easy/Strings/1678. Goal Parser Interpretation.py
@@ -15,3 +15,14 @@ class Solution:
         command = command.replace("()", "o")
         command = command.replace("(al)", "al")
         return command
+    
+"""This is a solution to the "Goal Parser Interpretation" problem. The problem asks to interpret a string command and return its interpretation. The interpretation is done based on some rules:
+
+If the character "G" is encountered, it should be interpreted as "G".
+If the characters "(al)" are encountered, they should be interpreted as "al".
+If the characters "()" are encountered, they should be interpreted as "o".
+The provided solution has two implementations. The first implementation (commented out) is using a loop to iterate through the string and build a new string based on the rules. The second implementation (uncommented) uses the replace() method of the string object to replace the appropriate substrings with their interpretations.
+
+The second implementation is more concise and arguably easier to read than the first implementation. It replaces the substring "()" with "o" and "(al)" with "al", which covers all the cases we need to interpret the command string.
+
+For example, if the input command is "G()(al)", the method will return "Goal". The first replace() call replaces the substring "()" with "o", resulting in the string "Go(al)". The second replace() call then replaces the substring "(al)" with "al", resulting in the final string "Goal". """
