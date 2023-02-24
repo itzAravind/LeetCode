f7353aadb5340e684eb90890c8a57ae67dc766f4
Srinivas Aravind <125225286+itzAravind@users.noreply.github.com>
2023-02-24 13:37:07 +0000

    Update 1859. Sorting the Sentence.py

--- Easy/Strings/1859. Sorting the Sentence.py
+++ Easy/Strings/1859. Sorting the Sentence.py
@@ -6,3 +6,11 @@ class Solution:
         for i in str1:
             result.append(i[1:][::-1])
         return " ".join(result)
+
+"""The given solution is a simple and efficient approach to sorting a sentence based on the index of each word. The time complexity of the solution is dominated by the sorting step, which takes O(N log N) time in the worst case, where N is the number of words in the sentence.
+
+The initial step of reversing the input string and splitting it into words takes O(N) time, where N is the length of the input string. However, this step is negligible compared to the sorting step, which dominates the time complexity of the algorithm.
+
+The final step of joining the sorted words together also takes O(N) time, where N is the number of words in the sentence. However, this step is also negligible compared to the sorting step.
+
+Overall, the time complexity of the solution is optimal for the given problem, as it is not possible to sort a list of words in less than O(N log N) time in the worst case. Therefore, the given solution is a good approach for sorting a sentence based on the index of each word."""
