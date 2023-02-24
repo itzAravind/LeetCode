2ed258816856d682256a1cd53f863476fe3d4302
Srinivas Aravind <125225286+itzAravind@users.noreply.github.com>
2023-02-24 14:49:51 +0000

    Update 1365. How Many Numbers Are Smaller Than the Current Number.py

--- Easy/Hash Map/1365. How Many Numbers Are Smaller Than the Current Number.py
+++ Easy/Hash Map/1365. How Many Numbers Are Smaller Than the Current Number.py
@@ -9,3 +9,9 @@ class Solution:
         for j in nums:
             result.append(dict[j])
         return result
+    
+"""This is a Python solution to the problem of finding the number of elements in a list that are smaller than the current element. The input is a list of integers "nums", and the output is a list of integers "result" where each element "result[i]" represents the number of elements in "nums" that are smaller than "nums[i]".
+
+The solution works by first creating a dictionary "dict" that maps each unique element in "nums" to its index in the sorted."""
+
+"""Overall time complexity is O(n log n) due to the sorting step, where "n" is the length of the input list "nums". The space complexity is O(n) because we create a dictionary with at most "n" key-value pairs. Note that the time complexity of dictionary lookups is expected O(1), which means on average it takes constant time to look up a key in a dictionary."""
