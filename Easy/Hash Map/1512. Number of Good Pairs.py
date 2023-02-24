c8100a8222174e04218c87be674c027fded568da
Srinivas Aravind <125225286+itzAravind@users.noreply.github.com>
2023-02-24 14:51:28 +0000

    Update 1512. Number of Good Pairs.py

--- Easy/Hash Map/1512. Number of Good Pairs.py
+++ Easy/Hash Map/1512. Number of Good Pairs.py
@@ -9,3 +9,11 @@ class Solution:
             else:
                 dict[i] = 1
         return counter
+    
+ """This is a Python code for a class Solution that contains a method numIdenticalPairs. This method takes a list of integers nums as input and returns an integer representing the number of identical pairs of elements in the list.
+
+The algorithm used here is to create a dictionary where the keys are the elements in the list, and the values are the number of occurrences of each element. The variable counter is initialized to zero, and as we iterate through the list, we check if the current element is already in the dictionary. If it is, we increment counter by the value of the corresponding key in the dictionary, which represents the number of pairs that can be formed using the current element and any of the previous occurrences of the same element. We then update the value of the corresponding key in the dictionary by adding 1 to it. If the current element is not in the dictionary, we add it to the dictionary with a value of 1.
+
+Finally, we return the value of counter, which represents the total number of identical pairs in the list.
+
+Note that the input parameter nums is expected to be a list of integers, and the output is an integer. Also, the use of the variable name dict is not recommended as it overrides the built-in Python function dict()."""
